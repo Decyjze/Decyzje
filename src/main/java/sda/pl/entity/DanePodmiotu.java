46645cea8ce80db1807fd15c52bfd55c051e36ec
Emil Czyrka <emilczyrka@gmail.com>
2019-08-24 11:07:56 +0200

    add Class Entity

---
+++ src/main/java/sda/pl/entity/DanePodmiotu.java
@@ -0,0 +1,15 @@
+package sda.pl.entity;
+
+public class DanePodmiotu {
+    private Long id;
+    private Long podmiotId;
+    private Long nrWariantu;
+    private String imie;
+    private String nazwisko;
+    private String pesel;
+    private String miasto;
+    private String nazwaUlicy;
+    private Integer nrDomu;
+    private Integer nrMieszkania;
+
+}
---
+++ src/main/java/sda/pl/entity/OznaczenieEntity.java
@@ -0,0 +1,11 @@
+package sda.pl.entity;
+
+public class OznaczenieEntity {
+    private Long id;
+    private Long decyzjaId;
+    private String numer;
+    private RodzajOznaczenia rodzajOznaczenia;
+    private Long zl2TpId;
+    private Stan stan;
+    private Long zl2TpId ;
+}

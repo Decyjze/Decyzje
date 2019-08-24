30829a0da45fcee1dd03a413eddb9ffd6eda2688
Emil Czyrka <emilczyrka@gmail.com>
2019-08-24 13:42:13 +0200

    add Emil

--- src/main/java/sda/pl/entity/OznaczenieEntity.java
+++ src/main/java/sda/pl/entity/OznaczenieEntity.java
@@ -1,6 +1,19 @@
 package sda.pl.entity;
 
 public class OznaczenieEntity {
+    private static Long staticId = 1L;
+
+    public OznaczenieEntity(Long id, Long decyzjaId, String numer, RodzajOznaczenia rodzajOznaczenia,
+                            Long zl2TpId, Stan stan) {
+        this.id = staticId;
+        staticId ++;
+        this.decyzjaId = decyzjaId;
+        this.numer = numer;
+        this.rodzajOznaczenia = rodzajOznaczenia;
+        this.zl2TpId = zl2TpId;
+        this.stan = stan;
+    }
+
     private Long id;
     private Long decyzjaId;
     private String numer;

d7fa480edc50d3640bd56689d1426d092610421c
Mateusz Gliwka <gliwson@gmail.com>
2019-08-24 14:10:12 +0200

    add dto mateusz

--- src/main/java/sda/pl/Main.java
+++ src/main/java/sda/pl/Main.java
@@ -3,23 +3,16 @@ package sda.pl;
 import sda.pl.connection.JsonToObject;
 import sda.pl.dto.WydajDecDto;
 import sda.pl.entity.DecyzjaEntity;
+import sda.pl.request.WydajDecyzjeRequest;
 
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        JsonToObject jsonToObject = new JsonToObject();
-        WydajDecDto jsontoWydajDec = jsonToObject.getJsontoWydajDec(new File("json/wydajDec.json"));
-
-        DecyzjaEntity decyzjaEntity = new DecyzjaEntity(jsontoWydajDec.getNumer(),jsontoWydajDec.getDataWaznosci(),
-                jsontoWydajDec.getDataWydania());
-
-
-
-        LocalDate dataWaznosci = decyzjaEntity.getDataWaznosci();
-        System.out.println(dataWaznosci);
+        WydajDecyzjeRequest wydajDecyzjeRequest = new WydajDecyzjeRequest("json/wydajDec.json");
 
 
 

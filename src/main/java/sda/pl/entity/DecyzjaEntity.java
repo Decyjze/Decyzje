759c81108e42113298fafc8e8bf76bd068e62c18
Emil Czyrka <emilczyrka@gmail.com>
2019-08-24 10:56:24 +0200

    edit  DecyzjaEntity

--- src/main/java/sda/pl/entity/DecyzjaEntity.java
+++ src/main/java/sda/pl/entity/DecyzjaEntity.java
@@ -1,12 +1,14 @@
 package sda.pl.entity;
 
 import java.time.LocalDateTime;
-
+//Emil
 public class DecyzjaEntity {
     private Long id;
     private String numer;
     private LocalDateTime dataWaznosci;
     private LocalDateTime dataWydania;
+
+
 
 
 }

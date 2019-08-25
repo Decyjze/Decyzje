2970b25457cf33a5b117e213d919b3b4d0a5be18
Emil Czyrka <emilczyrka@gmail.com>
2019-08-25 09:36:10 +0200

    add Emil

--- src/main/java/sda/pl/repository/Database.java
+++ src/main/java/sda/pl/repository/Database.java
@@ -2,15 +2,20 @@ package sda.pl.repository;
 
 import lombok.Getter;
 import sda.pl.entity.BlankietEntity;
+import sda.pl.entity.DanePodmiotu;
 
 import java.util.List;
 
 @Getter
 public class Database {
     private List<BlankietEntity> dataBaseBlankiet;
-
+    private List<DanePodmiotu> dataBaseDanePodmiotu;
     public boolean addDataBaseBlankiet(BlankietEntity blankietEntity) {
        return dataBaseBlankiet.add(blankietEntity);
     }
+    public boolean adddataBaseDanePodmiotu(DanePodmiotu danePodmiotu) {
+        return dataBaseDanePodmiotu.add(danePodmiotu);
+    }
+
 
 }

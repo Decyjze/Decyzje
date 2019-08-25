786d7590c2c0da5df39309fa92ccf48bb2419f15
Mateusz Gliwka <gliwson@gmail.com>
2019-08-25 10:00:37 +0200

    test Mateusz

--- src/main/java/sda/pl/dto/UchylDecyzje.java
+++ src/main/java/sda/pl/dto/UchylDecyzje.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDate;
